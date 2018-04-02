ea382d015e1a966b6da037c4516d138e19fc3182
Ji hye sung <zicomet8738@gmail.com>
2018-04-02 12:06:04 +0900

    mission 2

---
+++ Cube (1).h
@@ -0,0 +1,15 @@
+#pragma once
+#include <GL/glut.h>
+
+
+void drawCube(){
+	float color[3] = {3,2,0};
+	float size = 1.5;	
+	float pos[3] = {1,0,0};	
+
+	glPushMatrix();
+		glTranslatef(pos[0], pos[1], pos[2]);
+		glColor3f(color[0], color[1], color[2]);
+		glutSolidCube(size);
+	glPopMatrix();
+}
